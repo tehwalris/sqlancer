1339c773b0c3b6bd5997e6a18ac97b3e5741ee49
Philippe Voinov <philippevoinov@gmail.com>
2021-04-13 18:33:23 +0200

    Attempt to reproduce commit warning (doesn't work)

--- src/sqlancer/firebird/FirebirdProvider.java
+++ src/sqlancer/firebird/FirebirdProvider.java
@@ -2,23 +2,23 @@ package sqlancer.firebird;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.firebirdsql.management.FBManager;
 
 import sqlancer.AbstractAction;
-import sqlancer.IgnoreMeException;
 import sqlancer.Randomly;
 import sqlancer.SQLConnection;
 import sqlancer.SQLGlobalState;
 import sqlancer.SQLProviderAdapter;
-import sqlancer.StatementExecutor;
+import sqlancer.common.query.ExpectedErrors;
 import sqlancer.common.query.SQLQueryAdapter;
 import sqlancer.common.query.SQLQueryProvider;
 import sqlancer.firebird.FirebirdProvider.FirebirdGlobalState;
 import sqlancer.firebird.gen.FirebirdDeleteGenerator;
 import sqlancer.firebird.gen.FirebirdIndexGenerator;
 import sqlancer.firebird.gen.FirebirdInsertGenerator;
-import sqlancer.firebird.gen.FirebirdTableGenerator;
 import sqlancer.firebird.gen.FirebirdUpdateGenerator;
 import sqlancer.firebird.gen.FirebirdViewGenerator;
 
@@ -54,7 +54,8 @@ public class FirebirdProvider extends SQLProviderAdapter<FirebirdGlobalState, Fi
             return rand.getInteger(0, globalState.getOptions().getMaxNumberInserts() + 1);
         case CREATE_INDEX:
             return globalState.getDmbsSpecificOptions().testIndexes
-                    ? rand.getInteger(0, globalState.getDmbsSpecificOptions().maxNumIndexes + 1) : 0;
+                    ? rand.getInteger(0, globalState.getDmbsSpecificOptions().maxNumIndexes + 1)
+                    : 0;
         case DELETE:
             return rand.getInteger(0, globalState.getDmbsSpecificOptions().maxNumDeletes + 1);
         case UPDATE:
@@ -76,20 +77,59 @@ public class FirebirdProvider extends SQLProviderAdapter<FirebirdGlobalState, Fi
 
     @Override
     public void generateDatabase(FirebirdGlobalState globalState) throws Exception {
-        for (int i = 0; i < Randomly.fromOptions(1, globalState.getDmbsSpecificOptions().maxNumTables + 1); i++) {
-            boolean success;
-            do {
-                SQLQueryAdapter qt = new FirebirdTableGenerator().getQuery(globalState);
-                success = globalState.executeStatement(qt);
-            } while (!success);
+        // for (int i = 0; i < Randomly.fromOptions(1,
+        // globalState.getDmbsSpecificOptions().maxNumTables + 1); i++) {
+        // boolean success;
+        // do {
+        // SQLQueryAdapter qt = new FirebirdTableGenerator().getQuery(globalState);
+        // success = globalState.executeStatement(qt);
+        // } while (!success);
+        // }
+        // StatementExecutor<FirebirdGlobalState, Action> se = new
+        // StatementExecutor<>(globalState, Action.values(),
+        // FirebirdProvider::mapActions, (q) -> {
+        // if (globalState.getSchema().getDatabaseTables().isEmpty()) {
+        // throw new IgnoreMeException();
+        // }
+        // });
+        // se.executeStatements();
+
+        ExpectedErrors tableExpectedErrors = new ExpectedErrors();
+        FirebirdErrors.addTableErrors(tableExpectedErrors);
+        ExpectedErrors indexExpectedErrors = new ExpectedErrors();
+        FirebirdErrors.addIndexErrors(indexExpectedErrors);
+        ExpectedErrors insertExpectedErrors = new ExpectedErrors();
+        FirebirdErrors.addInsertErrors(insertExpectedErrors);
+
+        List<SQLQueryAdapter> queries = new ArrayList<SQLQueryAdapter>();
+        queries.add(new SQLQueryAdapter("CREATE TABLE t0(c0 INTEGER NOT NULL, c1 INTEGER, PRIMARY KEY(c0));",
+                tableExpectedErrors, true));
+        queries.add(new SQLQueryAdapter("CREATE TABLE t1(c0 FLOAT, c1 INTEGER, PRIMARY KEY(c1, c0));",
+                tableExpectedErrors, true));
+        queries.add(new SQLQueryAdapter("CREATE UNIQUE INDEX i0 ON T1(C0);", indexExpectedErrors, true));
+        queries.add(new SQLQueryAdapter("INSERT INTO T1 (C0) VALUES (-1.823638703E9);", insertExpectedErrors, false));
+        queries.add(new SQLQueryAdapter("INSERT INTO T1 (C1, C0) VALUES (436315175, 0.7288186586246848);",
+                insertExpectedErrors, false));
+        queries.add(new SQLQueryAdapter("INSERT INTO T1 (C1, C0) VALUES (1278918755, 0.8345087226113943);",
+                insertExpectedErrors, false));
+        queries.add(
+                new SQLQueryAdapter("INSERT INTO T1 (C0) VALUES (0.6784440174830495);", insertExpectedErrors, false));
+        queries.add(new SQLQueryAdapter("INSERT INTO T1 (C1, C0) VALUES (546268574, 0.6190884744047549);",
+                insertExpectedErrors, false));
+        queries.add(new SQLQueryAdapter("INSERT INTO T1 (C1) VALUES (662560035);", insertExpectedErrors, false));
+        queries.add(new SQLQueryAdapter("INSERT INTO T0 (C0, C1) VALUES (151221046, 138801703);", insertExpectedErrors,
+                false));
+        queries.add(new SQLQueryAdapter("INSERT INTO T1 (C0, C1) VALUES (0.575933333230997, 973502404);",
+                insertExpectedErrors, false));
+
+        for (SQLQueryAdapter query : queries) {
+            boolean success = globalState.executeStatement(query);
+            if (query.couldAffectSchema()) {
+                globalState.updateSchema();
+            }
+            System.out.print("success ");
+            System.out.println(success);
         }
-        StatementExecutor<FirebirdGlobalState, Action> se = new StatementExecutor<>(globalState, Action.values(),
-                FirebirdProvider::mapActions, (q) -> {
-                    if (globalState.getSchema().getDatabaseTables().isEmpty()) {
-                        throw new IgnoreMeException();
-                    }
-                });
-        se.executeStatements();
     }
 
     @Override
